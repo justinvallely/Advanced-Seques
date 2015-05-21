74be0312740d14bdea5394129fc40b8b6ee87234
Justin Vallely <justinvallely@gmail.com>
2015-05-20 23:18:13 -0600

    initial upload
    Transition back and forth as well as record which button initiated the
    segue.

---
+++ Advanced Seques/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Advanced Seques
+//
+//  Created by Justin Vallely on 5/20/15.
+//  Copyright (c) 2015 JMVapps. All rights reserved.
+//
+
+import UIKit
+
+var rowCounter:Int = 0
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        println(rowCounter)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
